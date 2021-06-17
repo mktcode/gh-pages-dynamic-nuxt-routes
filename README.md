2faa6f1309488322f148701f86b9ab0843062f39
mktcode <kontakt@markus-kottlaender.de>
2021-06-17 12:55:57 +0200

    making it a module (WIP)

---
+++ README.md
@@ -0,0 +1,13 @@
+# Dynamic Nuxt Routes for GitHub Pages
+
+```
+npm install --save gh-pages-dynamic-nuxt-routes
+```
+
+In `nuxt.config.js`:
+
+```js
+export default {
+  modules: [ 'gh-pages-dynamic-nuxt-routes' ]
+}
+```
--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-export default function ({ route, redirect }) {
-  const path = route.hash.replace('#', '')
-  redirect(path)
+export default function () {
+  const { nuxt, options } = this
+
+  options.router.middleware.push(({ route, redirect }) => {
+    const path = route.hash.replace('#', '')
+    redirect(path)
+  })
+
+  nuxt.hook('generate:distCopied', function (generator) {
+    console.log(generator.distPath)
+    console.log(generator.nuxt.options.app.basePath)
+  })
 }
